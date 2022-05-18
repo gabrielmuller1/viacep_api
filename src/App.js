d85faae47fda32527d91fb21506aa30b2a40f783
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2022-05-17 21:53:04 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InputCep from './components/InputCep';
+
+function App() {
+  return (
+    <div className="App">
+     <InputCep />
+    </div>
+  );
+}
+
+export default App;
